07d26a6bcd501a63e551de76be76efd4440566c1
gru-lucy <cbcrypto2017@gmail.com>
2024-10-13 13:40:30 +0300

    fix

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { StatCard } from "@/components/StatCard";
-import { columns } from "@/components/table/columns";
-import { DataTable } from "@/components/table/DataTable";
+// import { columns } from "@/components/table/columns";
+// import { DataTable } from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 
 const AdminPage = async () => {
