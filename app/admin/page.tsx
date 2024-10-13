bb89a82c1a8df672391a2a62e2b71a431cb808db
gru-lucy <cbcrypto2017@gmail.com>
2024-10-13 13:35:00 +0300

    fix

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -36,19 +36,19 @@ const AdminPage = async () => {
         <section className="admin-stat">
           <StatCard
             type="appointments"
-            count={appointments.scheduledCount}
+            count={appointments?.scheduledCount || 0}
             label="Scheduled appointments"
             icon={"/assets/icons/appointments.svg"}
           />
           <StatCard
             type="pending"
-            count={appointments.pendingCount}
+            count={appointments?.pendingCount || 0}
             label="Pending appointments"
             icon={"/assets/icons/pending.svg"}
           />
           <StatCard
             type="cancelled"
-            count={appointments.cancelledCount}
+            count={appointments?.cancelledCount || 0}
             label="Cancelled appointments"
             icon={"/assets/icons/cancelled.svg"}
           />
